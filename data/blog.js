bd835426f3d533d5ebb6c380ed286e3765f056fa
عمر احمد محمد عبد الفتاح <wolvthemes@gmail.com>
2025-04-25 08:10:48 +0300

    Update blog.js

--- data/blog.js
+++ data/blog.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     id: 1,
     imageSrc: "/images/blog/blog_img_09.jpg",
-    date: "23 Apr, 2022",
+    date: "23 Apr, 2024",
     title: "A Discount Cartridge dsu is Better Ever.",
     delay: "100",
     data: "<p>Hello anas bitch nigga</p>"
