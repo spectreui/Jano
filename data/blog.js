6ffb556ed1fd55ea808b8759da6fc33488b62ad8
Omar <wolvthemes@gmail.com>
2025-04-25 09:45:40 +0300

    M

--- data/blog.js
+++ data/blog.js
@@ -7,4 +7,13 @@ module.exports = [
     delay: "100",
     data: "<p>Hello anas bitch nigga</p>"
   },
+ 
+  {
+    id: 2,
+    imageSrc: "/images/blog/blog_img_09.jpg",
+    date: "23 Apr, 2024",
+    title: "A Duck is Better than a nigga.",
+    delay: "100",
+    data: "<p>Hello anas fukin</p>"
+  },
 ];
